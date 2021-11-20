97221e4a6b8e8f9cbf2deaad44350ad447e00041
Blue planet <30463977+Blueplanet20120@users.noreply.github.com>
2021-11-20 10:52:02 +0800

    update-sta

--- diy-part1.sh
+++ diy-part1.sh
@@ -45,33 +45,33 @@ ver419=`grep "LINUX_VERSION-4.19 ="  include/kernel-version.mk | cut -d . -f 3`
 ver54=`grep "LINUX_VERSION-5.4 ="  include/kernel-version.mk | cut -d . -f 3`
 ver510=`grep "LINUX_VERSION-5.10 ="  include/kernel-version.mk | cut -d . -f 3`
 if [ "$str1" = "5.4" ];then
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver54}_dev_Lenyu.img.gz
-  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver54}_uefi-gpt_dev_Lenyu.img.gz
+   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver54}_sta_Lenyu.img.gz
+  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver54}_uefi-gpt_sta_Lenyu.img.gz
 elif [ "$str1" = "4.19" ];then
-  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver419}_dev_Lenyu.img.gz
-  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver419}_uefi-gpt_dev_Lenyu.img.gz
+  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver419}_sta_Lenyu.img.gz
+  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver419}_uefi-gpt_sta_Lenyu.img.gz
 elif [ "$str1" = "4.14" ];then
-  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver414}_dev_Lenyu.img.gz
-  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver414}_uefi-gpt_dev_Lenyu.img.gz
+  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver414}_sta_Lenyu.img.gz
+  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver414}_uefi-gpt_sta_Lenyu.img.gz
 elif [ "$str1" = "5.10" ];then
-   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver510}_dev_Lenyu.img.gz
-  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver510}_uefi-gpt_dev_Lenyu.img.gz
+   mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver510}_sta_Lenyu.img.gz
+  mv  bin/targets/x86/64/openwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/openwrt_x86-64-${rename_version}_${str1}.${ver510}_uefi-gpt_sta_Lenyu.img.gz
 fi
 ls bin/targets/x86/64 | grep "_Lenyu.img" | cut -d - -f 3 | cut -d _ -f 1-2 > wget/op_version1
 #md5
-ls -l  "bin/targets/x86/64" | awk -F " " '{print $9}' > wget/open_dev_md5
-dev_version=`grep "_uefi-gpt_dev_Lenyu.img.gz" wget/open_dev_md5 | cut -d - -f 3 | cut -d _ -f 1-2`
-openwrt_dev=openwrt_x86-64-${dev_version}_dev_Lenyu.img.gz
-openwrt_dev_uefi=openwrt_x86-64-${dev_version}_uefi-gpt_dev_Lenyu.img.gz
+ls -l  "bin/targets/x86/64" | awk -F " " '{print $9}' > wget/open_sta_md5
+sta_version=`grep "_uefi-gpt_sta_Lenyu.img.gz" wget/open_sta_md5 | cut -d - -f 3 | cut -d _ -f 1-2`
+openwrt_sta=openwrt_x86-64-${sta_version}_sta_Lenyu.img.gz
+openwrt_sta_uefi=openwrt_x86-64-${sta_version}_uefi-gpt_sta_Lenyu.img.gz
 cd bin/targets/x86/64
-md5sum $openwrt_dev > openwrt_dev.md5
-md5sum $openwrt_dev_uefi > openwrt_dev_uefi.md5
+md5sum $openwrt_sta > openwrt_sta.md5
+md5sum $openwrt_sta_uefi > openwrt_sta_uefi.md5
 exit 0
 EOF
 
 cat>lenyu.sh<<-\EOOF
 #!/bin/bash
-lenyu_version="`date '+%y%m%d%H%M'`_dev_Len yu" 
+lenyu_version="`date '+%y%m%d%H%M'`_sta_Len yu" 
 echo $lenyu_version >  wget/DISTRIB_REVISION1 
 echo $lenyu_version | cut -d _ -f 1 >  files/etc/lenyu_version  
 #######
@@ -100,7 +100,7 @@ EOOF
 
 cat>files/usr/share/Check_Update.sh<<-\EOF
 #!/bin/bash
-# https://github.com/Lenyu2020/Actions-OpenWrt-x86
+# https://github.com/Blueplanet20120/Actions-OpenWrt-x86
 # Actions-OpenWrt-x86 By Lenyu 20210505
 #path=$(dirname $(readlink -f $0))
 # cd ${path}
@@ -114,16 +114,16 @@ fi
 rm -f /tmp/cloud_version
 # 获取固件云端版本号、内核版本号信息
 current_version=`cat /etc/lenyu_version`
-wget -qO- -t1 -T2 "https://api.github.com/repos/Lenyu2020/Actions-OpenWrt-x86/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g'  > /tmp/cloud_ts_version
+wget -qO- -t1 -T2 "https://api.github.com/repos/Blueplanet20120/Actions-OpenWrt-x86/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g'  > /tmp/cloud_ts_version
 if [ -s  "/tmp/cloud_ts_version" ]; then
 	cloud_version=`cat /tmp/cloud_ts_version | cut -d _ -f 1`
 	cloud_kernel=`cat /tmp/cloud_ts_version | cut -d _ -f 2`
 	#固件下载地址
 	new_version=`cat /tmp/cloud_ts_version`
-	DEV_URL=https://github.com/Lenyu2020/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_x86-64-${new_version}_dev_Lenyu.img.gz
-	DEV_UEFI_URL=https://github.com/Lenyu2020/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_x86-64-${new_version}_uefi-gpt_dev_Lenyu.img.gz
-	openwrt_dev=https://github.com/Lenyu2020/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_dev.md5
-	openwrt_dev_uefi=https://github.com/Lenyu2020/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_dev_uefi.md5
+	DEV_URL=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+	DEV_UEFI_URL=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+	openwrt_sta=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_sta.md5
+	openwrt_sta_uefi=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_sta_uefi.md5
 else
 	echo "请检测网络，查看是否能打开谷歌！"
 	exit 1
@@ -141,9 +141,9 @@ fi
 #md5值验证，固件类型判断
 if [ ! -d /sys/firmware/efi ];then
 	if [ "$current_version" != "$cloud_version" ];then
-		wget -P /tmp "$DEV_URL" -O /tmp/openwrt_x86-64-${new_version}_dev_Lenyu.img.gz
-		wget -P /tmp "$openwrt_dev" -O /tmp/openwrt_dev.md5
-		cd /tmp && md5sum -c openwrt_dev.md5
+		wget -P /tmp "$DEV_URL" -O /tmp/openwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+		wget -P /tmp "$openwrt_sta" -O /tmp/openwrt_sta.md5
+		cd /tmp && md5sum -c openwrt_sta.md5
 		if [ $? != 0 ]; then
       echo "您下载文件失败，请检查网络重试…"
       sleep 4
@@ -157,9 +157,9 @@ if [ ! -d /sys/firmware/efi ];then
 	fi
 else
 	if [ "$current_version" != "$cloud_version" ];then
-		wget -P /tmp "$DEV_UEFI_URL" -O /tmp/openwrt_x86-64-${new_version}_uefi-gpt_dev_Lenyu.img.gz
-		wget -P /tmp "$openwrt_dev_uefi" -O /tmp/openwrt_dev_uefi.md5
-		cd /tmp && md5sum -c openwrt_dev_uefi.md5
+		wget -P /tmp "$DEV_UEFI_URL" -O /tmp/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+		wget -P /tmp "$openwrt_sta_uefi" -O /tmp/openwrt_sta_uefi.md5
+		cd /tmp && md5sum -c openwrt_sta_uefi.md5
 		if [ $? != 0 ]; then
       echo "您下载文件失败，请检查网络重试…"
       sleep 4
@@ -186,11 +186,11 @@ case $num1 in
 	echo
 	sleep 3
 	if [ ! -d /sys/firmware/efi ];then
-		gzip -d openwrt_x86-64-${new_version}_dev_Lenyu.img.gz
-		sysupgrade /tmp/openwrt_x86-64-${new_version}_dev_Lenyu.img
+		gzip -d openwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+		sysupgrade /tmp/openwrt_x86-64-${new_version}_sta_Lenyu.img
 	else
-		gzip -d openwrt_x86-64-${new_version}_uefi-gpt_dev_Lenyu.img.gz
-		sysupgrade /tmp/openwrt_x86-64-${new_version}_uefi-gpt_dev_Lenyu.img
+		gzip -d openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+		sysupgrade /tmp/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img
 	fi
     ;;
     n|N)
@@ -199,11 +199,11 @@ case $num1 in
     echo
     sleep 3
 	if [ ! -d /sys/firmware/efi ];then
-		gzip -d openwrt_x86-64-${new_version}_dev_Lenyu.img.gz
-		sysupgrade -n  /tmp/openwrt_x86-64-${new_version}_dev_Lenyu.img
+		gzip -d openwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+		sysupgrade -n  /tmp/openwrt_x86-64-${new_version}_sta_Lenyu.img
 	else
-		gzip -d openwrt_x86-64-${new_version}_uefi-gpt_dev_Lenyu.img.gz
-		sysupgrade -n  /tmp/openwrt_x86-64-${new_version}_uefi-gpt_dev_Lenyu.img
+		gzip -d openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+		sysupgrade -n  /tmp/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img
 	fi
     ;;
     *)
@@ -246,7 +246,7 @@ EOF
 
 cat>files/usr/share/Lenyu-auto.sh<<-\EOF
 #!/bin/bash
-# https://github.com/Lenyu2020/Actions-OpenWrt-x86
+# https://github.com/Blueplanet20120/Actions-OpenWrt-x86
 # Actions-OpenWrt-x86 By Lenyu 20210505
 #path=$(dirname $(readlink -f $0))
 # cd ${path}
@@ -260,16 +260,16 @@ fi
 rm -f /tmp/cloud_version
 # 获取固件云端版本号、内核版本号信息
 current_version=`cat /etc/lenyu_version`
-wget -qO- -t1 -T2 "https://api.github.com/repos/Lenyu2020/Actions-OpenWrt-x86/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g'  > /tmp/cloud_ts_version
+wget -qO- -t1 -T2 "https://api.github.com/repos/Blueplanet20120/Actions-OpenWrt-x86/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g;s/v//g'  > /tmp/cloud_ts_version
 if [ -s  "/tmp/cloud_ts_version" ]; then
 	cloud_version=`cat /tmp/cloud_ts_version | cut -d _ -f 1`
 	cloud_kernel=`cat /tmp/cloud_ts_version | cut -d _ -f 2`
 	#固件下载地址
 	new_version=`cat /tmp/cloud_ts_version`
-	DEV_URL=https://github.com/Lenyu2020/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_x86-64-${new_version}_dev_Lenyu.img.gz
-	DEV_UEFI_URL=https://github.com/Lenyu2020/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_x86-64-${new_version}_uefi-gpt_dev_Lenyu.img.gz
-	openwrt_dev=https://github.com/Lenyu2020/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_dev.md5
-	openwrt_dev_uefi=https://github.com/Lenyu2020/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_dev_uefi.md5
+	DEV_URL=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+	DEV_UEFI_URL=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+	openwrt_sta=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_sta.md5
+	openwrt_sta_uefi=https://github.com/Blueplanet20120/Actions-OpenWrt-x86/releases/download/${new_version}/openwrt_sta_uefi.md5
 else
 	echo "请检测网络，查看是否能打开谷歌！"
 	exit 1
@@ -287,16 +287,16 @@ fi
 #md5值验证，固件类型判断
 if [ ! -d /sys/firmware/efi ];then
 	if [ "$current_version" != "$cloud_version" ];then
-		wget -P /tmp "$DEV_URL" -O /tmp/openwrt_x86-64-${new_version}_dev_Lenyu.img.gz
-		wget -P /tmp "$openwrt_dev" -O /tmp/openwrt_dev.md5
-		cd /tmp && md5sum -c openwrt_dev.md5
+		wget -P /tmp "$DEV_URL" -O /tmp/openwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+		wget -P /tmp "$openwrt_sta" -O /tmp/openwrt_sta.md5
+		cd /tmp && md5sum -c openwrt_sta.md5
 		if [ $? != 0 ]; then
 		  echo "您下载文件失败，请检查网络重试…"
 		  sleep 4
 		  exit
 		fi
-		gzip -d /tmp/openwrt_x86-64-${new_version}_dev_Lenyu.img.gz
-		sysupgrade /tmp/openwrt_x86-64-${new_version}_dev_Lenyu.img
+		gzip -d /tmp/openwrt_x86-64-${new_version}_sta_Lenyu.img.gz
+		sysupgrade /tmp/openwrt_x86-64-${new_version}_sta_Lenyu.img
 	else
 		echo -e "\033[32m 本地已经是最新版本，还更个鸡巴毛啊… \033[0m"
 		echo
@@ -304,16 +304,16 @@ if [ ! -d /sys/firmware/efi ];then
 	fi
 else
 	if [ "$current_version" != "$cloud_version" ];then
-		wget -P /tmp "$DEV_UEFI_URL" -O /tmp/openwrt_x86-64-${new_version}_uefi-gpt_dev_Lenyu.img.gz
-		wget -P /tmp "$openwrt_dev_uefi" -O /tmp/openwrt_dev_uefi.md5
-		cd /tmp && md5sum -c openwrt_dev_uefi.md5
+		wget -P /tmp "$DEV_UEFI_URL" -O /tmp/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+		wget -P /tmp "$openwrt_sta_uefi" -O /tmp/openwrt_sta_uefi.md5
+		cd /tmp && md5sum -c openwrt_sta_uefi.md5
 		if [ $? != 0 ]; then
 			echo "您下载文件失败，请检查网络重试…"
 			sleep 1
 			exit
 		fi
-		gzip -d /tmp/openwrt_x86-64-${new_version}_uefi-gpt_dev_Lenyu.img.gz
-		sysupgrade /tmp/openwrt_x86-64-${new_version}_uefi-gpt_dev_Lenyu.img
+		gzip -d /tmp/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img.gz
+		sysupgrade /tmp/openwrt_x86-64-${new_version}_uefi-gpt_sta_Lenyu.img
 	else
 		echo -e "\033[32m 本地已经是最新版本，还更个鸡巴毛啊… \033[0m"
 		echo
