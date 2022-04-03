ad8d035565f551a676e0c5f9e20238d646b42c5d
Lenyu2020 <59961153+Lenyu2020@users.noreply.github.com>
2022-04-03 21:50:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git small8 https://github.com/Lenyu2020/small-package' >>feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
 mkdir -p files/usr/share
 mkdir -p files/etc/
