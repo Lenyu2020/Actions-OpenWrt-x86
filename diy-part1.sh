d0ea6ef1b96f60f8dba3a62eb04114c5a6a79243
Lenyu2020 <59961153+Lenyu2020@users.noreply.github.com>
2024-09-17 11:24:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -455,9 +455,9 @@ if [ "$installed_version" = "$version" ]; then
   exit 0
 fi
 
-# If versions do not match, prompt user for confirmation
-echo_orange "你即将更新passwall为最新版本：$version，确定更新吗？(回车默认y，否则输入n)"
-read -r confirmation
+# If versions do not match, prompt user for confirmation with a 10-second countdown
+echo_orange "你即将更新passwall为最新版本：$version，确定更新吗？(回车默认y，10秒后自动执行y)"
+read -t 10 -r confirmation
 confirmation=${confirmation:-y}
 
 if [ "$confirmation" != "y" ]; then
